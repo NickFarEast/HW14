bd89ba9c4775f2bd092dab5152f7191d90ad3377
NickFarEast <shramko.dt@gmail.com>
2022-02-21 17:54:58 +1000

    выполнен шаг 2

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ def get_by_title(title: str):
           WHERE n.title = '{title}' AND n.date_added = (SELECT max(date_added)
           FROM netflix n 
           WHERE n.title = '{title}')
+          AND n.type = 'Movie'
           """
     result = []
     for item in functions.get_result(sql):
@@ -22,10 +23,30 @@ def get_by_title(title: str):
             "release_year": item.get("release_year"),
             "genre": item.get("listed_in"),
             "description": item.get("description")
-            }
+        }
         result.append(s)
 
-    print(result)
+    return app.response_class(response=json.dumps(result),
+                              status=200,
+                              mimetype="application/json")
+
+
+@app.route("/movie/<year1>/to/<year2>")
+def get_by_year(year1: str, year2: str):
+    sql = f"""SELECT *
+          FROM netflix n 
+          WHERE n.release_year BETWEEN {year1} AND {year2}
+          AND n.type = 'Movie'
+          LIMIT 100
+          """
+    result = []
+    for item in functions.get_result(sql):
+        s = {
+            "title": item.get("title"),
+            "release_year": item.get("release_year"),
+        }
+        result.append(s)
+
     return app.response_class(response=json.dumps(result),
                               status=200,
                               mimetype="application/json")
